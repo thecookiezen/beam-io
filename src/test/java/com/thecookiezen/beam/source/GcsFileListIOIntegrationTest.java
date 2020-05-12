81eec35d2f1cd41dcd4f8f72d7926ecf07f91b74
Korneliusz Rabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-05-12 23:12:17 +0200

    BEAM-5: Uses bytebuddy to mock GcsUtil instance for tests

--- src/test/java/com/thecookiezen/beam/source/GcsFileListIOIntegrationTest.java
+++ src/test/java/com/thecookiezen/beam/source/GcsFileListIOIntegrationTest.java
@@ -4,41 +4,52 @@ import com.google.api.client.http.apache.v2.ApacheHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.SslUtils;
 import com.google.api.services.storage.Storage;
-import com.google.api.services.storage.model.Objects;
 import junit.framework.TestCase;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import org.apache.beam.sdk.extensions.gcp.options.GcsOptions;
 import org.apache.beam.sdk.extensions.gcp.util.GcsUtil;
 import org.apache.beam.sdk.extensions.gcp.util.RetryHttpRequestInitializer;
+import org.apache.beam.sdk.extensions.gcp.util.gcsfs.GcsPath;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.values.PCollection;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.MethodRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
-import java.io.IOException;
 import java.security.GeneralSecurityException;
+
+import static net.bytebuddy.implementation.MethodDelegation.to;
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 @RunWith(JUnit4.class)
 public class GcsFileListIOIntegrationTest extends TestCase {
 
     @Rule
+    public MethodRule agentAttachmentRule = new AgentAttachmentRule();
+
+    @Rule
+    public final transient TestPipeline pipeline = TestPipeline.create().enableAbandonedNodeEnforcement(false);
+
+    @Rule
     public GenericContainer gcsServer = new GenericContainer<>("fsouza/fake-gcs-server")
             .withExposedPorts(4443)
-            .withClasspathResourceMapping("gcsFiles", "/data/gcsFiles", BindMode.READ_ONLY);
-
-    private final GcsOptions gcsOptions = PipelineOptionsFactory.create().as(GcsOptions.class);
+            .withClasspathResourceMapping("gcs-files", "/data/gcs-files", BindMode.READ_ONLY);
 
     @Before
     public void setUp() throws GeneralSecurityException {
         String address = "127.0.0.1";
         Integer port = gcsServer.getFirstMappedPort();
 
-        System.out.println(address);
-        System.out.println(port);
+        final GcsOptions gcsOptions = PipelineOptionsFactory.create().as(GcsOptions.class);
 
         ApacheHttpTransport httpTransport = new ApacheHttpTransport(ApacheHttpTransport.newDefaultHttpClientBuilder()
                 .setSSLSocketFactory(new SSLConnectionSocketFactory(SslUtils.trustAllSSLContext()))
@@ -50,7 +61,7 @@ public class GcsFileListIOIntegrationTest extends TestCase {
                 .setRootUrl("https://" + address + ":" + port)
                 .setSuppressAllChecks(true);
 
-        final GcsUtil gcsUtil = GcsUtil.GcsUtilFactory.create(
+        GcsUtilInterceptor.gcsUtil = GcsUtil.GcsUtilFactory.create(
                 gcsOptions,
                 storage.build(),
                 storage.getHttpRequestInitializer(),
@@ -58,15 +69,22 @@ public class GcsFileListIOIntegrationTest extends TestCase {
                 gcsOptions.getGcsUploadBufferSizeBytes()
         );
 
-        gcsOptions.setGcsUtil(gcsUtil);
+        new ByteBuddy()
+                .redefine(GcsUtil.GcsUtilFactory.class)
+                .method(named("create"))
+                .intercept(to(GcsUtilInterceptor.class))
+                .make()
+                .load(GcsUtil.GcsUtilFactory.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
     }
 
     @Test
-    public void should_test_integration() throws IOException {
-        GcsUtil gcsUtil = gcsOptions.getGcsUtil();
+    @AgentAttachmentRule.Enforce(redefinesClasses = true)
+    public void should_test_integration() {
+        PCollection<String> output = pipeline
+                .apply(GcsFileListIO.fromPath(GcsPath.fromUri("gs://gcs-files/*")));
 
-        Objects objects = gcsUtil.listObjects("gcsFiles", "test", null);
+        PAssert.that(output).containsInAnyOrder("test1.txt", "test2.txt", "anotherTest.txt");
 
-        System.out.println(objects.size());
+        pipeline.run();
     }
 }
